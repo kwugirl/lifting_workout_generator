20850ef2070e98b7a8db4ef997ff26a91359ab3d
Katherine Wu <kwu@newrelic.com>
2016-10-16 08:22:13 -0700

    Create PullExercises class

---
+++ pull_exercises.rb
@@ -0,0 +1,52 @@
+require './movement'
+
+class PullExercise
+  def self.create_new
+    [PullUp, RingRow, PauseRingRow].sample.new
+  end
+end
+
+class PullUp < Movement
+  attr_reader :assistance_method, :grip
+
+  def initialize
+    super
+    @assistance_method = assistance_method_options.sample
+    @grip = grip_options.sample
+    @name = "#{assistance_method} #{grip}"
+  end
+
+  def assistance_method_options
+    ["low bar", "band assisted", "jumping"]
+  end
+
+  def grip_options
+    ["pull-up", "chin-up"]
+  end
+
+  def rep_scheme_options
+    ["3x5", "5x5", "5x8", "6x5"]
+  end
+end
+
+class RingRow < Movement
+  def initialize
+    super
+    @name = "ring row"
+  end
+
+  def rep_scheme_options
+    ["6x4", "5x8"]
+  end
+end
+
+class PauseRingRow < Movement
+  def initialize
+    super
+    @name = "pause ring row"
+  end
+
+  def rep_scheme_options
+    ["3x6", "5x5"]
+  end
+end
---
+++ pull_exercises_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require './exercises'
+require './pull_exercises'
+
+# pullup = PullUp.new
+# puts Exercise.new(pullup, pullup.rep_scheme)
+
+# ring_row = RingRow.new
+# puts Exercise.new(ring_row, ring_row.rep_scheme)
+
+# pause_ring_row = PauseRingRow.new
+# puts Exercise.new(pause_ring_row, pause_ring_row.rep_scheme)
+
+puts PullExercise.create_new.inspect
+puts PullExercise.create_new.inspect
+puts PullExercise.create_new.inspect
