dba55412b403f78a0b42387cccc137556168312e
Katherine Wu <kwu@newrelic.com>
2016-12-05 10:08:02 -0800

    Save comments on how much to bump up by each week

--- models/weekly_workout_generator_test.rb
+++ models/weekly_workout_generator_test.rb
@@ -1,11 +1,11 @@
 require 'minitest/autorun'
 require './weekly_workout_generator'
 
-squat_5rm = 150
+squat_5rm = 155 # bump by 5 when ready to go up
 press_type = :bench
-bench_5rm = 97
-shoulder_press_5rm = 74
-deadlift_5rm = 225
+bench_5rm = 97 # bump by 5 when ready to go up
+shoulder_press_5rm = 74 # bump by 2 when ready to go up
+deadlift_5rm = 230 # bump by 5 when ready to go up
 plan = WeeklyWorkoutPlan.new(squat_5rm, press_type, bench_5rm, shoulder_press_5rm, deadlift_5rm)
 
 puts plan.to_s
