a1a3f3af9cd391e2e43361af08ec81066542f13d
Katherine Wu <kwu@newrelic.com>
2017-01-09 17:28:25 -0800

    Move workout printing to different route

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require_relative 'models/weekly_workout_generator'
 
 class LiftingWorkoutGenerator < Sinatra::Base
   get '/' do
+    'hi again'
+  end
+
+  get '/workout' do
     squat_5rm = 155 # bump by 5 when ready to go up
     press_type = :bench
     bench_5rm = 97 # bump by 5 when ready to go up
@@ -10,6 +14,6 @@ class LiftingWorkoutGenerator < Sinatra::Base
     deadlift_5rm = 230 # bump by 5 when ready to go up
     plan = WeeklyWorkoutPlan.new(squat_5rm, press_type, bench_5rm, shoulder_press_5rm, deadlift_5rm)
 
-    erb :index, locals: {plan: plan}
+    erb :workout, locals: {plan: plan}
   end
 end
